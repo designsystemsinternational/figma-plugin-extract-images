f146b7a86dc5e06a6894c54a9a68b312adf625d1
Martin Bravo <bravomartin@gmail.com>
2020-08-06 20:01:14 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Collect images
+
+#### A Figma Plugin to find all images on a file and extract the originals to a zip file.
+
+This plugin goes through the whole file finding images, and collects them on a zip file for download.
+
+This is useful when you used many images and don't have easy access to the original images (ie. moodboard, website mockup), and want to extract the original files.
+
+## How to use
+
+This version has no options and has no UI. Just run the plugin and you will be prompted to download a zip file. Large files may take a while.
+
+## Development
+
+After cloning the repo, run `npm install`.
+
+Then, to compile run `npm run dev`
+
+To link this plugin to Figma go to Menu > Plugins > Development > New Plugin... and choose **Link existing plugin** / _Click to choose a manifest.json file_ and find this folder's `manifest.json`
---
+++ src/code.js
@@ -0,0 +1,67 @@
+const slugify = require('slugify');
+
+figma.ui.onmessage = (message) => {
+  if (message.close) {
+    figma.closePlugin();
+  }
+};
+
+const getAllImages = async () => {
+  const allRectanglesWithImages = figma.root.findAll((node) => {
+    return (
+      node.type === 'RECTANGLE' &&
+      node.fills.some((fill) => fill.type == 'IMAGE')
+    );
+  });
+
+  const allImages = allRectanglesWithImages.reduce((acc, rect) => {
+    let name = rect.name;
+
+    return acc.concat(
+      rect.fills.map((fill, i) => {
+        const name = rect.name + (rect.fills.length == 1 ? '' : `-${i}`);
+        return {
+          name: uniqueName(acc, name),
+          path: getPath(rect),
+          hash: fill.imageHash,
+        };
+      })
+    );
+  }, []);
+
+  console.log('Found %s images...', allImages.length);
+
+  figma.showUI(__html__, { visible: false });
+  figma.ui.postMessage({ filename: figma.root.name });
+  for (let i = 0; i < allImages.length; i++) {
+    const image = allImages[i];
+    const data = figma.getImageByHash(image.hash);
+    image.bytes = await data.getBytesAsync();
+    figma.ui.postMessage({ image });
+  }
+  figma.ui.postMessage({ done: true });
+};
+
+const uniqueName = (list, name, i = 1) => {
+  const versioned = name + `-${i}`;
+  if (list.includes((a) => a.name == name)) {
+    if (list.includes((a) => a.name == versioned)) {
+      return uniqueName(list, name, i + 1);
+    } else {
+      return versioned;
+    }
+  } else {
+    return name;
+  }
+};
+
+const getPath = (obj, path = []) => {
+  if (obj.parent) {
+    path.unshift(obj.parent.name);
+    return getPath(obj.parent, path);
+  } else {
+    return path.join('/');
+  }
+};
+
+getAllImages();
